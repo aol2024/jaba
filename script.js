df555bd17fa5f5b393baa8e793fbe851bc892f84
Aol 2024 <157704090+aol2024@users.noreply.github.com>
2024-09-07 23:50:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const popup = document.querySelector('.popup');
+    const acceptBtn = document.querySelector('#acceptBtn');
+    const declineBtn = document.querySelector('#declineBtn');
+    const affiliateLink = 'https://4533bx35oj3kbyc2md360hys4c.hop.clickbank.net';
+
+    // Função para redirecionar ao interagir com a página
+    function redirectToAffiliate() {
+        window.location.href = affiliateLink;
+    }
+
+    // Redirecionar ao clicar no botão de aceitar
+    acceptBtn.addEventListener('click', redirectToAffiliate);
+
+    // Redirecionar ao clicar no botão de decline
+    declineBtn.addEventListener('click', redirectToAffiliate);
+
+    // Redirecionar ao mover o mouse, rolar ou clicar na página
+    //document.addEventListener('mousemove', redirectToAffiliate);
+    //document.addEventListener('scroll', redirectToAffiliate);
+    //document.addEventListener('click', redirectToAffiliate);
+});
